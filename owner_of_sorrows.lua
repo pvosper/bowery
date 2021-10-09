ebb6ec2d0dd057ff84f5ed805b9010768127133d
pvosper <pvosper@mac.com>
2021-10-09 07:57:50 -0700

    Updating for two voices
    Using inputs for both timing and pitch; adding second set of functions for second voice

--- owner_of_sorrows.lua
+++ owner_of_sorrows.lua
@@ -7,42 +7,52 @@
 -- out4: ar voice 2
 
 output[1].scale = {2, 4, 5, 7, 9, 10, 12}
-output[2].scale = {2, 5, 9, 12}
-
-function vos(value, offset, scale_factor)
-    return (value + offset) / scale_factor
-end
-
-function new_ar_time()
-   return math.random(1, 5) / 10
-end
+output[3].scale = {2, 5, 9, 12}
 
 function new_level()
-    return math.random(50, 100) / 10
+    return math.random(3, 10)
 end
 
 -- on TT: 'CROW.C1 n'
 function ii.self.call1(tt_value)
+    print("-- start --")
    repeats = tt_value
-   play_note()
+   play_note_voice_1()
+   play_note_voice_2()
 end
 
-play_note = function()
-   ar_time = new_ar_time()
+play_note_voice_1 = function()
+   ar_time = input[2].volts / 11
    level = new_level()
-   volts_1 = input[2].volts
+   volts_1 = input[1].volts / 2
    output[1].volts = volts_1
-   output[2] (ar(ar_time * .075, ar_time * .925, level, 'logarithmic'))
-   output[3] (pulse())
+   output[2] (ar(ar_time * .1, ar_time * .9, level, 'linear'))
 end
 
 output[2].done = function()
     if(repeats > 0)
     then
         repeats = repeats - 1
-        print(repeats)
-        play_note()
+        play_note_voice_1()
     else
-        print("-- done --")
+        print("-- 1 done --")
+    end
+end 
+
+play_note_voice_2 = function()
+   ar_time = input[2].volts / 5
+   level = new_level()
+   volts_1 = input[1].volts / 2
+   output[3].volts = volts_1
+   output[4] (ar(ar_time * .2, ar_time * .8, level, 'linear'))
+end
+
+output[4].done = function()
+    if(repeats > 0)
+    then
+        repeats = repeats - 1
+        play_note_voice_2()
+    else
+        print("-- 2 done --")
     end
 end 
