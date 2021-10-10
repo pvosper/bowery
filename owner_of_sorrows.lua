eeb0230555619458c5cbed5a7c6e7ec3d5db31cb
pvosper <pvosper@mac.com>
2021-10-10 07:25:03 -0700

    Expanding TT control; minor tweaks
    - function ii.self.call3(tt1, tt2, tt3)

--- owner_of_sorrows.lua
+++ owner_of_sorrows.lua
@@ -10,23 +10,25 @@ output[1].scale = {2, 4, 5, 7, 9, 10, 12}
 output[3].scale = {2, 5, 9, 12}
 
 function new_level()
-    return math.random(3, 10)
+    return math.random(10)
 end
 
--- on TT: 'CROW.C1 n'
-function ii.self.call1(tt_value)
+-- on TT: 'CROW.C3 75 47 21'
+function ii.self.call3(tt1, tt2, tt3)
     print("-- start --")
-   repeats = tt_value
-   play_note_voice_1()
-   play_note_voice_2()
+    repeats = tt1
+    ar_factor_1 = tt2
+    ar_factor_2 = tt3
+    play_note_voice_1()
+    play_note_voice_2()
 end
 
 play_note_voice_1 = function()
-   ar_time = input[2].volts / 11
-   level = new_level()
-   volts_1 = input[1].volts / 2
-   output[1].volts = volts_1
-   output[2] (ar(ar_time * .1, ar_time * .9, level, 'linear'))
+    ar_time = input[2].volts / ar_factor_1
+    level = new_level()
+    volts_1 = input[1].volts / 2
+    output[1].volts = volts_1
+    output[2] (ar(ar_time * .1, ar_time * .9, level, 'linear'))
 end
 
 output[2].done = function()
@@ -40,11 +42,11 @@ output[2].done = function()
 end 
 
 play_note_voice_2 = function()
-   ar_time = input[2].volts / 5
-   level = new_level()
-   volts_1 = input[1].volts / 2
-   output[3].volts = volts_1
-   output[4] (ar(ar_time * .2, ar_time * .8, level, 'linear'))
+    ar_time = input[2].volts / ar_factor_2
+    level = new_level()
+    volts_1 = input[1].volts / 3
+    output[3].volts = volts_1
+    output[4] (ar(ar_time * .2, ar_time * .8, level, 'linear'))
 end
 
 output[4].done = function()
